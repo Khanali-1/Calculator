cc2273e27644da5b079f726f28fbed9429a73e34
unknown <meo24693@gmail.com>
2025-12-28 11:02:03 -0800

    added new files and code

---
+++ script.js
@@ -0,0 +1,25 @@
+// Calculator 
+
+
+const display = document.getElementById("display");
+
+function appendtoDisplay(input){
+    display.value += input;
+
+} 
+
+function clearDisplay(){
+    display.value = "";
+
+}
+
+function calculate(){
+    try{
+    display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+
+    }
+
+}
